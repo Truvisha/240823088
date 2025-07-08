ce044e8ad35da8afa19fd7512f98a40f13dd4d42
Truvisha <truvishasavaliya244@gmail.com>
2025-07-09 01:34:31 +0530

    simpal node js program

--- index.js
+++ index.js
@@ -13,13 +13,144 @@
 // }
 
 // console.log("program.2 find maximum of two number")
-var a=5
-var b=9
-if(a>b)
-{
-    console.log("maximum number is:",a)
-}
-else
-{
-    console.log("maximum number is:",b)
-}
+// var a=5
+// var b=9
+// if(a>b)
+// {
+//     console.log("maximum number is:",a)
+// }
+// else
+// {
+//     console.log("maximum number is:",b)
+// }
+
+// console.log("program.3 check leap year")
+// var year=2020
+// if(year/4)
+// {
+//     console.log(year,"is leap year")
+// }
+// else
+// {
+//     console.log(year,"is not leap yaer")
+// }
+
+// console.log("program.4 sum of natural number")
+// var n=5
+// sum=0
+// for(i=1;i<=n;i++)
+// {
+//     sum+=i
+// }
+// console.log(sum)
+
+// console.log("program.5 factorial of a number")
+// var n=4
+// fact=1
+// for(i=1;i<=n;i++)
+// {
+//     fact*=i
+// }
+// console.log(fact)
+
+// console.log("program.6 print multiplication table")
+// var n=3
+// for(i=1;i<=10;i++)
+// {
+//     console.log(`${n} X ${i} = ${n*i}`)
+// }
+
+// console.log("program.7 reverse number")
+// var n=322142
+// var rev=n.toString().split('').reverse().join('')
+// console.log(rev)
+
+// console.log("program.8 check palindrome number")
+// var n=121
+// var p=n.toString().split('').reverse().join('')
+// if(n==p)
+// {
+//     console.log(n,"number is palindrome")
+// }
+// else
+// {
+//     console.log(n,"number is not palindrome")
+// }
+
+// console.log("program.9 check prime number")
+// var n=11
+// var prime=1
+// for(i=2;i<=n/2;i++)
+// {
+//     if(n%i==0)
+//     {
+//         prime=0
+//         break
+//     }
+// }
+// if(prime)
+// {
+//     console.log(n,"is prime number")
+// }
+// else
+// {
+//     console.log(n,"is not prime number")
+// }
+
+// console.log("program.10 count digits in a number")
+// var n=5023
+// var count=0
+// if(n==0)
+// {
+//     count=1;
+// }
+// else
+// {
+//     if(n<0)
+//     {
+//         n+=n
+//     }
+//     else(n!=0)
+//     {
+//         count++
+//     }
+// }
+// console.log("number of digits:",n)
+
+// console.log("program.11 sum of digits")
+// var n=123
+// var sum=0
+// var digit=0
+
+// while(n)
+// {
+//     digit=n%10
+//     sum+=digit
+//     n=(n-digit)/10
+// }
+// console.log("sum of digits:",n)
+
+// console.log("program.19 number is positive,negative or zero")
+// var n=-5
+// if(n>0)
+// {
+//     console.log("number is positive")
+// }
+// else if(n<0)
+// {
+//     console.log("number is negative")
+// }
+// else
+// {
+//     console.log("number is zero")
+// }
+
+// console.log("program.20 find power(Exponentiation)")
+// var a=2
+// var b=3
+// var result=1
+// for(i=0;i<b;i++)
+// {
+//     result*=a
+// }
+// console.log("power:",result)
