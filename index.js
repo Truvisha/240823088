ebf44321d1e1d19729b49c127514706bf9ee2ef2
Truvisha <truvishasavaliya244@gmail.com>
2025-07-09 00:26:02 +0530

    program of maximum number

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-const name="Truvisha"
-console.log("hello "+name)
+// const name="Truvisha"
+// console.log("hello "+name)
 
-console.log("program.1 even odd number")
-const n=4
-if(n%2)
+// console.log("program.1 even odd number")
+// const n=4
+// if(n%2)
+// {
+//     console.log("odd number")
+// }
+// else
+// {
+//     console.log("even number")
+// }
+
+// console.log("program.2 find maximum of two number")
+var a=5
+var b=9
+if(a>b)
 {
-    console.log("odd number")
+    console.log("maximum number is:",a)
 }
 else
 {
-    console.log("even number")
+    console.log("maximum number is:",b)
 }
