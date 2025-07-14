e7772ceae192d1c636523245f1d2e8f58ca4c8ae
Truvisha <truvishasavaliya244@gmail.com>
2025-07-14 09:25:39 +0530

    init with basic program

---
+++ index.js
@@ -0,0 +1,13 @@
+const name="Truvisha"
+console.log("hello "+name)
+
+console.log("program.1 even odd number")
+const n=4
+if(n%2)
+{
+    console.log("odd number")
+}
+else
+{
+    console.log("even number")
+}
