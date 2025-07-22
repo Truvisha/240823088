6453a8c9f2c55b819ecaaf98d72b1bb61d363f7d
Truvisha <truvishasavaliya244@gmail.com>
2025-07-22 11:06:04 +0530

    Add files via upload

---
+++ module.js
@@ -0,0 +1,35 @@
+// import add from "./add.js"
+// const add=require("./add.js")
+// console.log(add(11,22))
+
+
+//PARH
+//npm install path
+// const path=require("path")
+// console.log(path.basename(__dirname+"myfile.txt"))
+// console.log("Directory Name:",__dirname)
+// console.log("full path:",path.resolve(__dirname),"myfile.txt")
+
+//MOMENT
+//npm install moment
+// const moment=require("moment")
+// console.log("Current date and time:",moment().format("DD-MM-YYYY HH:mm:ss"))
+
+//LODASH
+//npm install lodash
+// const ls=require("lodash")
+// console.log("Capitalize:",ls.capitalize("truvisha savaliya"))
+
+
+//FS
+//npm install fs
+// const fs=require("fs")
+// fs.writeFile("myfile.txt","Hello Student!",(err)=>{
+// if(err)throw err;
+// console.log("file hase been saved!")
+// })
+
+// fs.readFile("myfile.txt","utf8",(err,data)=>{
+//     if(err)throw err;
+//     console.log("file content:",data)
+//     })
